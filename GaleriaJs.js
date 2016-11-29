4f3901ccb2015bb00d86180758093541bfe07924
anjonnes <dacamarro@hotmail.com>
2016-11-29 15:06:51 -0500

    GaleriaJs.1

---
+++ GaleriaJs.js
@@ -0,0 +1,136 @@
+window.onload=carpag;
+
+var geg="https://s-media-cache-ak0.pinimg.com/736x/90/7e/74/907e74ebc15ebc919c8ea7cf5433acbb.jpg";
+
+function carpag(){
+
+var u1=document.getElementById("1");
+u1.addEventListener("mouseenter",function(){
+clicEn(u1.src)
+});
+  
+u1.addEventListener("mouseleave",function() {
+clic();
+});
+  
+u1.addEventListener("click",function(){
+echg(u1.src)
+});    
+  
+var u2=document.getElementById("2");
+u2.addEventListener("mouseenter",function(){
+clicEn(u2.src)
+});
+  
+u2.addEventListener("mouseleave",function() {
+clic();
+});
+  
+u2.addEventListener("click",function(){
+echg(u2.src)
+});    
+  
+var u3=document.getElementById("3");
+u3.addEventListener("mouseenter",function(){
+clicEn(u3.src)
+});
+  
+u3.addEventListener("mouseleave",function() {
+clic();
+});
+  
+u3.addEventListener("click",function(){
+echg(u3.src)
+});    
+  
+var u4=document.getElementById("4");
+u4.addEventListener("mouseenter",function(){
+clicEn(u4.src)
+});
+  
+u4.addEventListener("mouseleave",function() {
+clic();
+});
+  
+u4.addEventListener("click",function(){
+echg(u4.src)
+});    
+  
+var u5=document.getElementById("5");
+u5.addEventListener("mouseenter",function(){
+clicEn(u5.src)
+});
+  
+u5.addEventListener("mouseleave",function() {
+clic();
+});
+  
+u5.addEventListener("click",function(){
+echg(u5.src)
+});  
+  
+var u6=document.getElementById("6");
+u6.addEventListener("mouseenter",function(){
+clicEn(u6.src)
+});
+  
+u6.addEventListener("mouseleave",function() {
+clic();
+});
+  
+u6.addEventListener("click",function(){
+echg(u6.src)
+});    
+  
+var u7=document.getElementById("7");
+u7.addEventListener("mouseenter",function(){
+clicEn(u7.src)
+});
+  
+u7.addEventListener("mouseleave",function() {
+clic();
+});
+  
+u7.addEventListener("click",function(){
+echg(u7.src)
+});  
+  
+var u8=document.getElementById("8");
+u8.addEventListener("mouseenter",function(){
+clicEn(u8.src)
+});
+  
+u8.addEventListener("mouseleave",function() {
+clic();
+});
+  
+u8.addEventListener("click",function(){
+echg(u8.src)
+});
+  
+var u9=document.getElementById("9");
+u9.addEventListener("mouseenter",function(){
+clicEn(u9.src)
+});
+  
+u9.addEventListener("mouseleave",function() {
+clic();
+});
+  
+u9.addEventListener("click",function(){
+echg(u9.src)
+});
+}
+  
+function clicEn(url){
+var ig=document.getElementById("i0").src=url;
+}
+
+function echg(fij){
+geg=fij 
+var gl=document.getElementById("i0").src=geg; 
+}
+
+function clic(ft){
+var nqq=document.getElementById("i0").src=geg;
+}
